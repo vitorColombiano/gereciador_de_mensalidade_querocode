5efb0dd0328115959b6fb2ae4008c06fa2976567
Vitor Colombiano SIlva <150254320+vitorColombiano@users.noreply.github.com>
2024-07-02 09:28:52 -0300

    Crud completo de Student (#3)
    * listar estudantes
    
    * created student
    
    * delete student
    
    * update student

---
+++ app/controllers/api/v1/students_controller.rb
@@ -0,0 +1,64 @@
+module Api
+    module V1
+        class StudentsController < ApplicationController
+            def index
+                page = params[:page].to_i > 0 ? params[:page].to_i : 1
+                count = params[:count].to_i > 0 ? params[:count].to_i : 10
+
+                students = Student.order('created_at ASC').page(page).per(count)
+                formatted_student = students.map do |student|
+                    {
+                        id: student.id,
+                        name: student.name,
+                        cpf: student.cpf,
+                        birthdate: student.birthdate.strftime('%d/%m/%Y'),
+                        payment_method: student.payment_method
+                    }
+                end
+
+                render json: { page: page, items: formatted_student }, status: :ok
+            end
+
+            def create
+                student = Student.new(student_params)
+
+                if student.save
+                    render json: { id: student.id }, status: :ok
+                else
+                    render json: { errors: student.errors.full_messages }, status: :unprocessable_entity
+                end
+            end
+
+            def update
+                student = Student.find(params[:id])
+
+                if student.update(payment_method_params)
+                    render json: { id: student.id, payment_method: student.payment_method, name: student.name }, status: :ok
+                else
+                    render json: { errors: student.errors.full_messages }, status: :unprocessable_entity 
+                end
+
+            end
+
+            def destroy
+                student = Student.find(params[:id])
+
+                if student.destroy
+                    render json: { message: "Student deleted successfully" }, status: :ok
+                else
+                    render json: { errors: student.errors.full_messages }, status: :unprocessable_entity 
+                end
+            end
+
+            private
+
+            def student_params
+                params.require(:student).permit(:name, :cpf, :birthdate, :payment_method)
+            end
+
+            def payment_method_params
+                params.require(:student).permit(:payment_method)
+            end
+        end
+    end
+end
