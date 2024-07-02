b2afa24d1cca6c6009980a3be8d8c72095fb2820
vitorColombiano <vicolombiano69@gmail.com>
2024-07-02 13:31:01 -0300

    criar matricula

--- app/controllers/api/v1/enrollments_controller.rb
+++ app/controllers/api/v1/enrollments_controller.rb
@@ -6,7 +6,51 @@ module Api
                 count = params[:count].to_i > 0 ? params[:count].to_i : 10
 
                 enrollments = Enrollment.includes(:bills).page(page).per(count)
-                formatted_enrollment = enrollments.map do |enrollment|
+                formatted_enrollment = enrollments.map { |enrollment| enrollment_json(enrollment) }
+                end
+                render json: { page: page, item: formatted_enrollment }, status: :ok
+            end
+
+            def create
+                enrollment = Enrollment.new(enrollment_params)
+
+                if enrollment.save
+                    crete_bills(enrolment)
+                    render json: enrollment_json(enrollment), status: :created
+                else
+                    render json: { errors: enrollment.errors.full_messages }, status: :unprocessable_entity
+                end
+            end
+
+            private
+
+            def enrollment_params
+                params.require(:enrollment).permit(:amout, :installments, :due_day, :student_id)
+            end
+
+            def crete_bills(enrollment)
+                amount_per_bill = enrollment.amout / enrollment.installments
+                due_day = enrollment.due_day
+                current_day = Date.today
+
+                enrollment.installments.times do |i|
+                    due_date = if due_date < current_day.day
+                        (current_date + (i+1).months).change(day: due_day)
+                    else
+                        (current_date + i.months).change(day: due_day)
+                    end
+                    
+                    enrollment.bills.create(
+                        amount: amount_per_bill,
+                        due_date: due_date,
+                        status: 'open'
+                    )
+                end
+            end
+
+
+            def enrollment_json(enrollment)
+                {
                     {
                         id: enrollment.id,
                         student_id: enrollment.student_id,
@@ -23,8 +67,7 @@ module Api
                             }
                         end
                     }
-                end
-                render json: { page: page, item: formatted_enrollment }, status: :ok
+                }
             end
         end
     end
