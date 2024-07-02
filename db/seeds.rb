ea1eb5a3d841cf26e8b937ab3b80eb92e7f7f95c
Vitor Colombiano <vicolombiano69@gmail.com>
2024-07-02 08:27:52 -0300

    abastecendo o banco

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,26 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'faker'
+
+5.times do
+    Student.create(
+      {name: Faker::Name.name,
+      cpf: Faker::IdNumber.brazilian_citizen_number(formatted: true),
+      birthdate: Faker::Date.birthday(min_age: 18, max_age: 65),
+      payment_method: %w(credit_card boleto).sample}
+    )
+end
+
+5.times do
+    enrollment = Enrollment.create!(
+      amount: Faker::Number.between(from: 100, to: 10000),
+      installments: Faker::Number.between(from: 2, to: 12),
+      due_day: Faker::Number.between(from: 1, to: 31),
+      student_id: Student.pluck(:id).sample
+    )
+    enrollment.installments.times do |i|
+        enrollment.bills.create!(
+            amount: enrollment.amount / enrollment.installments,
+            due_date: Date.today.next_month(i).change(day: enrollment.due_day),
+            status: 'open'
+        )
+    end
+end
